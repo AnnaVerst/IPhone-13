659129c4bcd28265252cef42451a047bd5056b79
AnnaVerst <89072215+AnnaVerst@users.noreply.github.com>
2022-01-22 17:14:43 +0500

    Add files via upload

--- script/scroll.js
+++ script/scroll.js
@@ -1,13 +1,18 @@
-
+const scrollFunc = () => {                                      //внутри функции создаем переменные и работют только в этой функции
+    
 const links = document.querySelectorAll('.header-menu__item a')  // объявляем переменные из меню
-const link = document.querySelectorAll('.card-details__link-characteristics')
+const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
 
-links.forEach((element)=> {                                      //задаем ссылкам массиву функцию (=>)
+const newArray = [...links, linkCharacteristics]
+
+console.log(newArray);
+
+newArray.forEach((element)=> {                                   //задаем ссылкам массиву функцию (=>)
     element.addEventListener('click', (event) => {               // addEventListener(возможность добавлять нескольео обработчиков) добавляем обработчик КЛИК и какое то действие(event), далее прописываем функцию
         event.preventDefault()                                   // отменяем преброс с ссылки на секцию
 
         const id = element.getAttribute('href').substring(1)     // по клику выводится индетификатор (#present) substring - убирает решетку
-        const section = document.getElementById(id)               // находим секцию куда будет вести ссылка (<section class="present" id="present">)б ищется ТОЛЬКО по ИНДИФИКАТОРУ id
+        const section = document.getElementById(id)              // находим секцию куда будет вести ссылка (<section class="present" id="present">)б ищется ТОЛЬКО по ИНДИФИКАТОРУ id
             
         if (section) {
             section.scrollIntoView({                              // scrollIntoView переводит резко на нужную секцию
@@ -21,20 +26,5 @@ links.forEach((element)=> {                                      //задаем 
 })
 
 
-link.forEach((element) => {
-    element.addEventListener('click', (event)=> {
-        event.preventDefault()
-
-        const id = element.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
-
-        if(section) {
-            section.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
-        }else{
-            console.log('нет');
-        }
-    })
-})
+}
+scrollFunc()
